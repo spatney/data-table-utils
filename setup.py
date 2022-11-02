406ca0855f0c3ea513ed4bf2dbe965efc85f5d93
Sachin Patney <spatney@users.noreply.github.com>
2022-11-01 23:25:57 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='data_table_utils',
-    version='0.0.2',
+    version='0.0.3',
     author='Sachin Patney',
     author_email='admin@nope.com',
     description='Utils for data-table.com',
@@ -18,5 +18,5 @@ setuptools.setup(
     },
     license='MIT',
     packages=['data_table_utils'],
-    install_requires=['redis==4.3.4', 'pika==1.3.0'],
+    install_requires=['redis==4.3.4', 'pika==1.3.1'],
 )
